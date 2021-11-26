a4e94be79491aa7a34ecb5e9f1d4bfb3016eff2d
Felix Albrigtsen <felixalbrigtsen@gmail.com>
2021-11-26 14:02:06 +0100

    Rebuild log checker

--- miniban.sh
+++ miniban.sh
@@ -49,60 +49,59 @@ function banCheck() {
     #Associative array.  Key=ip addr, value=number of occurances
     declare -A IPLOG
     #Step through the logfile from start to end. Alternatively, we could use journalctl
-    while read line; do
+    line=$1
     
-    	#The first 15 chars have the format of "Jan 01 00:00:00", parse them to unix time
-        timestring=$( echo "$line" | cut -c1-16 )  
-        timestamp=$( date --date "$timestring" +%s )
-        if [[ $timestamp -lt $banTime ]]; then
-            #This entry is older than the ban period, skip it
+    #The first 15 chars have the format of "Jan 01 00:00:00", parse them to unix time
+    timestring=$( echo "$line" | cut -c1-16 )  
+    timestamp=$( date --date "$timestring" +%s )
+    if [[ $timestamp -lt $banTime ]]; then
+        #This entry is older than the ban period, skip it
+        return
+    fi
+    
+    #Successful logins, reset the counter
+    if [[ "$line" == *"Accepted password"* ]] ; then
+        #Line looks like: "Nov 23 15:39:04 miniban sshd[44135]: Accepted password for root from 10.24.3.54 port 53514 ssh2"
+        ip=$( echo $line | sed 's/.*from //; s/port.*//' ) #Get text between "from " and "port"
+        #Set the ip's counter to 0. Does not fail even if the value is not already initialized
+        IPLOG[$ip]=0
+    fi
+    #Authentication failure, increment the counter
+    if [[ "$line" == *"authentication failure"* ]]; then
+        
+
+        #Use awk to look at each "column" of the line. Return the line if it starts with "rhost="
+        rhost=$( echo $line | awk '{for(j=1;j<=NF;j++){if($j~/^rhost=/){print $j}}}' )
+        if [[ $rhost != "rhost="* ]] ; then
+            echo "Log parsing failed"
             continue
         fi
-        
-		#Successful logins, reset the counter
-		if [[ "$line" == *"Accepted password"* ]] ; then
-			#Line looks like: "Nov 23 15:39:04 miniban sshd[44135]: Accepted password for root from 10.24.3.54 port 53514 ssh2"
-			ip=$( echo $line | sed 's/.*from //; s/port.*//' ) #Get text between "from " and "port"
-			#Set the ip's counter to 0. Does not fail even if the value is not already initialized
-         	IPLOG[$ip]=0
-		fi
-    	#Authentication failure, increment the counter
-        if [[ "$line" == *"authentication failure"* ]]; then
-            
+        ip=${rhost#*=} #Grab everything after "="
 
-            #Use awk to look at each "column" of the line. Return the line if it starts with "rhost="
-			rhost=$( echo $line | awk '{for(j=1;j<=NF;j++){if($j~/^rhost=/){print $j}}}' )
-            if [[ $rhost != "rhost="* ]] ; then
-                echo "Log parsing failed"
-                continue
-            fi
-            ip=${rhost#*=} #Grab everything after "="
-
-            #Check that the ip is a valid ipv4 address
-            if [[ ! $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] ; then
-                #Invalid ip address, skip it
-                continue
-            fi
-
-            #First login attempt fails
-            if [[ "$line" == *"pam_unix(sshd:auth): authentication failure;"* ]]; then
-                ((IPLOG[$ip]++))
-            fi
-
-            #We assume that the default PAM configuration is in use, with maximum 3 attempts
-            #and log lines matching the following two cases:
-
-            #One additional attempt
-            if [[ "$line" == *"PAM 1 more authentication"* ]]; then
-                ((IPLOG[$ip]++))
-            fi
-            #Two additional attempt
-            if [[ "$line" == *"PAM 2 more authentication"* ]]; then
-                ((IPLOG[$ip] += 2))
-            fi
-
+        #Check that the ip is a valid ipv4 address
+        if [[ ! $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] ; then
+            #Invalid ip address, skip it
+            continue
         fi
-    done < "$1"
+
+        #First login attempt fails
+        if [[ "$line" == *"pam_unix(sshd:auth): authentication failure;"* ]]; then
+            ((IPLOG[$ip]++))
+        fi
+
+        #We assume that the default PAM configuration is in use, with maximum 3 attempts
+        #and log lines matching the following two cases:
+
+        #One additional attempt
+        if [[ "$line" == *"PAM 1 more authentication"* ]]; then
+            ((IPLOG[$ip]++))
+        fi
+        #Two additional attempt
+        if [[ "$line" == *"PAM 2 more authentication"* ]]; then
+            ((IPLOG[$ip] += 2))
+        fi
+
+    fi
 
     #Display table
     for ip in "${!IPLOG[@]}"; do 
@@ -113,4 +112,14 @@ function banCheck() {
     done;
 }
 
-$(  journalctl -fu ssh -n0 | banCheck )
+
+
+prevLog=$( journalctl -u ssh -n 1 | grep -e "authentication failure" -e "Accepted password" )
+# Continously loop through the very last line of the ssh log, bancheck if it relates to authentication
+while : ; do
+    newLog=$( journalctl -u ssh -n 1 | grep -e "authentication failure" -e "Accepted password" )
+    if [[ $prevLog != $newLog ]] ; then
+        banCheck $newLog
+    fi
+    prevLog=$newLog
+done
